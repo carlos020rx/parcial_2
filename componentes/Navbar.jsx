1a7020a889b7867a511d5cc698c28f80a1b6fbc6
carlos020rx <carlos_andres.garzon@uao.edu.co>
2024-10-03 18:16:20 -0500

    se agrego una carpeta images donde van las imagenes y se cambio  la url del banner

--- componentes/Navbar.jsx
+++ componentes/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
                 <h2>Carlos Andres Garzon Guerrero</h2>
             </div>
             <div className="nav-right">
-                <a href="https://twitter.com/carlos020rx" target="_blank" rel="noopener noreferrer">@carlos020rx</a>
+                <a href="https://www.instagram.com/carlos020rx?igsh=cGQ2NXlsdG02c24y" target="_blank" rel="noopener noreferrer">@carlos020rx</a>
                 {/* Puedes añadir más enlaces a redes sociales si lo necesitas */}
             </div>
         </nav>
